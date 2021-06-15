847cb9b71706f8cd65d9098e4ab3ddc4cc86df7c
sidneyrdm <sidneybritomal@gmail.com>
2021-06-15 17:03:25 -0300

    adicionando dependencia mockito e realizando teste com mock2

--- src/test/java/com/test/ExceptionTEst.java
+++ src/test/java/com/test/ExceptionTEst.java
@@ -1,2 +1,45 @@
-package com.test;public class ExceptionTEst {
+package com.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class ExceptionTEst {
+/*
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void empty() {
+        new ArrayList<Object>().get(0);
+    }*/
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shouldTestExceptionMessage() throws IndexOutOfBoundsException{
+
+        List<Object> list = new ArrayList<Object>();
+
+        thrown.expect(IndexOutOfBoundsException.class);
+        thrown.expectMessage("Index: 0, Size: 0");
+        list.get(0);
+    }
+
+    @Test
+    public void testExceptionMessage(){
+
+        try{
+            new ArrayList<Object>().get(0);
+            fail("Esperado que IndexOutOfBoundsException seja lançada");
+        }catch (IndexOutOfBoundsException ex){
+            assertThat(ex.getMessage(), is("Index: 0, Size: 0"));
+        }
+    }
 }
--- src/test/java/com/test/TestCalculatorMock.java
+++ src/test/java/com/test/TestCalculatorMock.java
@@ -1,2 +1,23 @@
-package com.test;public class TestCalculatorMock {
+package com.test;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestCalculatorMock {
+
+    @Test
+    public void testSomarComMock(){
+        Calculator calculator = mock(Calculator.class);
+
+        when(calculator.somar("1+2")).thenReturn(10);
+        
+        int resultado = calculator.somar("1+2");
+
+        assertEquals(10, resultado);
+    }
+
 }
